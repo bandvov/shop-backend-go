4d4041d39dda58d9b3dc86eff565948c10dc7e43
bandvov <bandvov@gmail.com>
2023-07-23 12:04:13 +0300

    refactored queries

--- handlers.go
+++ handlers.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/jackc/pgx/v4"
 )
 
 type Handlers struct{}
@@ -12,7 +14,9 @@ func (h *Handlers) getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "This is my website!\n")
 }
-func (h *Handlers) getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
+func (h *Handlers) getHello(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("got /hello request\n")
+		io.WriteString(w, "Hello, HTTP!\n")
+	}
 }
--- queries.go
+++ queries.go
@@ -1,14 +1,19 @@
 package main
 
-var CreateTableQuery = `CREATE TABLE apply_leave1 (
-	leaveid serial PRIMARY KEY NOT NULL ,
-	mdays double precision NOT NULL DEFAULT 0 ,
-	leavetype varchar(20) NOT NULL DEFAULT '' ,
-	daytype text NOT NULL '',
-	leavefrom timestamp with time zone NOT NULL,
-	leaveto timestamp with time zone NOT NULL,
-	applieddate timestamp with time zone NOT NULL,
-	leavestatus varchar(15) NOT NULL DEFAULT ''  ,
-	resultdate timestamp with time zone,
-	certificatestatus bool NOT NULL DEFAULT FALSE 
-	certificate json NULL)`
+type Queries struct{}
+
+func (q *Queries) CreateTableQuery() string {
+
+	return `CREATE TABLE apply_leave1 (
+		leaveid serial PRIMARY KEY NOT NULL ,
+		mdays double precision NOT NULL DEFAULT 0 ,
+		leavetype varchar(20) NOT NULL DEFAULT '' ,
+		daytype text NOT NULL '',
+		leavefrom timestamp with time zone NOT NULL,
+		leaveto timestamp with time zone NOT NULL,
+		applieddate timestamp with time zone NOT NULL,
+		leavestatus varchar(15) NOT NULL DEFAULT ''  ,
+		resultdate timestamp with time zone,
+		certificatestatus bool NOT NULL DEFAULT FALSE 
+		certificate json NULL)`
+}
