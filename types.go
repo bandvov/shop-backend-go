a345bfb7f0ab8e1be01369e5ee700e4540d4e69c
bandvov <bandvov@gmail.com>
2023-08-06 20:24:05 +0300

    update types

--- types.go
+++ types.go
@@ -1,6 +1,13 @@
 package main
 
+import "time"
+
 type User struct {
-	UserId int    `db:"user_id"`
-	Email  string `db:"email"`
+	UserId    int       `db:"user_id" json:"user_id"`
+	FullName  string    `db:"full_name" json:"full_name"`
+	Email     string    `db:"email" json:"email"`
+	Phone     string    `db:"phone" json:"phone,omitempty"`
+	Password  string    `db:"password" json:"password"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
 }
