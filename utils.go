7272117ffcf0e07db930648c149eaa8670802ec3
bandvov <bandvov@gmail.com>
2023-08-06 20:23:47 +0300

    add utils

--- utils.go
+++ utils.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func getEnvVariable(name string) (string, error) {
@@ -13,4 +17,33 @@ func getEnvVariable(name string) (string, error) {
 		return "", errors.New(fmt.Sprintf(errMessage, name))
 	}
 	return connString, nil
+}
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func capitalize(str string) string {
+	runes := []rune(str)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
+}
+
+func validate(body interface{}, fields ...string) (bool, []error) {
+
+	var bodyMap map[string]interface{}
+	inrec, _ := json.Marshal(body)
+	json.Unmarshal(inrec, &bodyMap)
+
+	// var errors []error
+	for _, v := range fields {
+
+		fmt.Println(bodyMap[v])
+	}
+	return true, nil
 }
