4dca6a317ebb2d6ee6f7a22695e761b1feff1882
bandvov <bandvov@gmail.com>
2023-08-06 20:24:56 +0300

    update handlers

--- handlers.go
+++ handlers.go
@@ -25,25 +25,34 @@ func (h *Handlers) getHello(conn *pgx.Conn) func(w http.ResponseWriter, r *http.
 }
 func (h *Handlers) addUser(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var users = make([]*User, 0)
+		defer r.Body.Close()
+		if r.Body == http.NoBody {
+			log.Println("No body")
+			http.Error(w, "No body", http.StatusBadRequest)
+			return
+		}
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			log.Printf("Error reading body: %v\n", err)
+			http.Error(w, "can't read body", http.StatusBadRequest)
+			return
+		}
+		fmt.Println(user)
+		hashedPassword, err := hashPassword(user.Password)
+		if err != nil {
+			log.Printf("Error while hashing: %v\n", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		user.Password = hashedPassword
+		validate(user, "email", "phone", "password")
 
-		rows, err := conn.Query(context.Background(), `INSERT INTO users (email) values ($1) RETURNING *;`, "new")
+		_, err = conn.Query(context.Background(), `INSERT INTO users (email, phone, full_name, password) values ($1, $2, $3, $4) RETURNING *;`, &user.Email, &user.Phone, &user.FullName, &user.Password)
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer rows.Close()
-
-		for rows.Next() {
-			user := &User{}
-			if err := rows.Scan(&user.UserId, &user.Email); err != nil {
-				log.Fatalf("Scan: %v", err)
-			}
-
-			users = append(users, user)
-		}
-		marshaled, err := json.Marshal(users)
-
-		w.Write(marshaled)
+		w.Write([]byte("User created"))
 	}
 
 }
@@ -59,7 +68,7 @@ func (h *Handlers) getUsers(conn *pgx.Conn) func(w http.ResponseWriter, r *http.
 
 		for rows.Next() {
 			user := &User{}
-			if err := rows.Scan(&user.UserId, &user.Email); err != nil {
+			if err := rows.Scan(&user.UserId, &user.Email, &user.Password, &user.Phone, &user.FullName); err != nil {
 				log.Fatalf("Scan: %v", err)
 			}
 
