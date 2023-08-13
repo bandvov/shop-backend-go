1200cbc79d9fbfeb608f2cf2a0e34da588c3ea92
bandvov <bandvov@gmail.com>
2023-08-13 16:02:51 +0300

    update types

--- types.go
+++ types.go
@@ -4,10 +4,10 @@ import "time"
 
 type User struct {
 	UserId    int       `db:"user_id" json:"user_id"`
-	FullName  string    `db:"full_name" json:"full_name"`
-	Email     string    `db:"email" json:"email"`
+	Email     string    `db:"email" json:"email" validate:"required"`
+	Password  string    `db:"password" json:"password" validate:"required"`
 	Phone     string    `db:"phone" json:"phone,omitempty"`
-	Password  string    `db:"password" json:"password"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	FullName  string    `db:"full_name" json:"full_name,omitempty"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty"`
 }
